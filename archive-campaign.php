d76288352b92d67505eb4a6d5465d78f8524de75
てぃー <laketown.web@gmail.com>
2023-12-01 20:17:32 +0900

    Zoom面談前の修正

--- archive-campaign.php
+++ archive-campaign.php
@@ -97,15 +97,6 @@ $sitemap = esc_url( home_url( '/sitemap/' ) );
           </ul>
           <div class="campaign__wp-pagenavi wp-pagenavi">
             <?php wp_pagenavi(); ?>
-            <!-- <?php the_posts_pagination(
-                array(
-                    'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
-                    'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
-                    'prev_text'     => ('<'), // 「前へ」リンクのテキスト
-                    'next_text'     => ('>'), // 「次へ」リンクのテキスト
-                    'type'          => 'list', // 戻り値の指定 (plain/list)
-                )
-            ); ?> -->
           </div>
 
         </div>
